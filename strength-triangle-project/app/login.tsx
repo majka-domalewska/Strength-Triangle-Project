a0a640c4cbc7c3f9a4b8f433211de1afccc5f107
Maja Domalewska <162155232+majka-domalewska@users.noreply.github.com>
2024-11-06 14:28:10 +0100

    Removed.env

---
+++ strength-triangle-project/app/login.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import LoginSignupScreen from "@/components/loginSignupScreen";
+
+export default function Index() {
+  return (
+    <View>
+      <LoginSignupScreen />
+    </View>
+  );
+}
---
+++ strength-triangle-project/components/initialChoice.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { View, TextInput, Button, Text, Alert } from 'react-native';
+
